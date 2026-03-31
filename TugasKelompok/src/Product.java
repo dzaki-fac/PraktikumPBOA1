8b161f586c9aa9af7f2c3481ee99507443b46236
Dzaki <dzxkyaf@gmail.com>
2026-03-31 20:59:11 +0700

    nambahin nama

--- TugasKelompok/src/Product.java
+++ TugasKelompok/src/Product.java
@@ -1,3 +1,9 @@
+// Disusun oleh:
+// Daniel Lamganda Tua G.	(24060124120048)
+// Dehar Zaidan Dzaki A.	(24060124130099)
+// Dzaki Fathul’Alim Cahyo	(24060124130103)
+// Elang Fadila Ahmad	    (24060124130108)
+
 public class Product {
     private String id;
     private String name;
